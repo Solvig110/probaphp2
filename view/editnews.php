a00a9cdddf6111302264196cf0599575e9e4ba5c
Solvig110 <margosmi@gmail.com>
2014-12-10 18:58:28 +0300

    4 commit

--- view/editnews.php
+++ view/editnews.php
@@ -7,10 +7,10 @@
 
 
          <form action="" name="" method="">
-              <h1 >Измените заголовок</h1>
-            <input type="text"  name="title" value="<?=$article['title'];?>">
+              <h2 >Измените заголовок</h2>
+            <input type="text"  name="title"> <?=$article['title'];?>
             <h3 >Измените текст</h3>
-            <textarea name="text" ><?=$article['text'];?></textarea>
+            <textarea name="text"rows="10" cols="45" ><?=$article['text'];?></textarea>
             <input  type="submit" value="Сохранить изменения">
          </form>
 
