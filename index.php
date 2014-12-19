b552331e37f1e9e7f0057058ed2e23e70a75aabe
Solvig110 <margosmi@gmail.com>
2014-12-19 12:19:11 +0300

    дз 4

--- index.php
+++ index.php
@@ -1,7 +1,22 @@
 <?php
 
-require_once __DIR__ . '/models/news.php';
+//require_once __DIR__ . '/models/news.php';
 
-$news = News_getAll();
+//$news = News_getAll();
 
-include 'view/index.php';
+//include 'view/index.php';
+
+
+require 'boot.php';
+//роутер
+//r=news/all
+$route = $_GET['r'];
+$routeParts = explode('/',$route);
+$controllerClassName = ucfirst($routeParts[0].'Controller');
+$actionName=$routeParts[1];
+//фронтконтроллер
+$controller = new $controllerClassName;
+$controller->action($routeParts[1]);
+//$controller = new NewsContrloller();
+//$controller->('all');
+
